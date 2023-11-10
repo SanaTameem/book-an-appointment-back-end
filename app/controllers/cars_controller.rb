f78b34045d95078d9df189bbf3df7704c28ea9ce
Melkamu Almawu Ayen <malmawu@gmail.com>
2023-11-10 15:31:20 +0100

    Fix linters error

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,20 +1,22 @@
 class CarsController < ApplicationController
-    def create
-        @car = Car.new(car_params)
-        if @car.save
-            render json: @car, status: :created, location: @car
-        else
-            render json: { error: 'Car data is not recorded!!' }
-        end
+  def create
+    @car = Car.new(car_params)
+    if @car.save
+      render json: @car, status: :created, location: @car
+    else
+      render json: { error: 'Car data is not recorded!!' }
     end
-    def update
-        if @car.update(car_params)
-            render json: @car
-        else
-        render json: @car.errors, status: :unprocessable_entity
-        end
+  end
+
+  def update
+    if @car.update(car_params)
+      render json: @car
+    else
+      render json: @car.errors, status: :unprocessable_entity
     end
-    def car_params
-        params.require(:car).permit(:name, :image, :brand, :reservation_fee)
-    end
+  end
+
+  def car_params
+    params.require(:car).permit(:name, :image, :brand, :reservation_fee)
+  end
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,9 +1,10 @@
 class Car < ApplicationRecord
-
-    validates :name, presence: true
-    validates :image, presence: true
-    validates :brand, presence: true
-    validates :reservation_fee, presence :true, numericality: {
-        greather_than_or_equal_to: 1, less_than_or_equal_to: 10000
-    }
+  MAX_VALUE = 10_000
+  MIN_VALUE = 1
+  validates :name, presence: true
+  validates :image, presence: true
+  validates :brand, presence: true
+  validates :reservation_fee, presence: true, numericality: {
+    greather_than_or_equal_to: MIN_VALUE, less_than_or_equal_to: MAX_VALUE
+  }
 end

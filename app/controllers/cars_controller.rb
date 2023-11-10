1d008e7a662b219bfeda02f375c0643d89c9a9d8
Melkamu Almawu Ayen <malmawu@gmail.com>
2023-11-10 15:16:37 +0100

    implement the controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -8,7 +8,11 @@ class CarsController < ApplicationController
         end
     end
     def update
-
+        if @car.update(car_params)
+            render json: @car
+        else
+        render json: @car.errors, status: :unprocessable_entity
+        end
     end
     def car_params
         params.require(:car).permit(:name, :image, :brand, :reservation_fee)

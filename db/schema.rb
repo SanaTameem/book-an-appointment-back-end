b050a26843785ebc9734b21bf16f340b62aedc22
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-11-14 13:06:29 +0500

    Add authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_10_114544) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_14_043852) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,12 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_10_114544) do
     t.integer "total_amount", null: false
     t.text "discription"
     t.integer "duration", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

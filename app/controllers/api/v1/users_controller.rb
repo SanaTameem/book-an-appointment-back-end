5c90a5ca7614eafe13c2e7994fd155e19aa504b2
Melkamu Almawu Ayen <malmawu@gmail.com>
2023-11-15 23:07:59 +0900

    implmenting deleting reservation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,32 +1,37 @@
-# class Api::V1::UsersController < ApplicationController
-#   def sign_up
-#     @user = User.find_or_initialize_by(name: user_params[:name])
+class Api::V1::UsersController < ApplicationController
+  def index
+    @users = User.all
+    render json: @users
+  end
 
-#     if @user.save
-#       render json: {
-#         status: { code: 200, message: 'signed up successfuly',
-#                   data: @user }
-#       }, status: :ok
-#     else
-#       render json: { error: @user.errors.full_messages.join(', ') }, status: :unprocessable_entity
-#     end
-#   end
+  def sign_up
+    @user = User.find_or_initialize_by(name: user_params[:name])
 
-#   def login
-#     @user = User.find_by(name: user_params[:name])
+    if @user.save
+      render json: {
+        status: { code: 200, message: 'signed up successfuly',
+                  data: @user }
+      }, status: :ok
+    else
+      render json: { error: @user.errors.full_messages.join(', ') }, status: :unprocessable_entity
+    end
+  end
 
-#     if @user
-#       render json: {
-#         status: { code: 200, message: 'signed in successfuly', data: @user }
-#       }, status: :ok
-#     else
-#       render json: { error: 'User not found' }, status: :not_found
-#     end
-#   end
+  def login
+    @user = User.find_by(name: user_params[:name])
 
-#   private
+    if @user
+      render json: {
+        status: { code: 200, message: 'signed in successfuly', data: @user }
+      }, status: :ok
+    else
+      render json: { error: 'User not found' }, status: :not_found
+    end
+  end
 
-#   def user_params
-#     params.require(:user).permit(:name)
-#   end
-# end
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+end

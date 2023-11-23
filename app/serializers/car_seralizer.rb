af83b39d1db9d09a239d4d332360a62dc72d4d60
Melkamu Almawu Ayen <malmawu@gmail.com>
2023-11-23 05:41:32 -0500

    Modify the seralizer

--- app/serializers/car_seralizer.rb
+++ app/serializers/car_seralizer.rb
@@ -1,6 +1,4 @@
 class CarSerializer
-  include ActiveModel::Serializer
-
   attributes :id, :name, :image, :model, :finance_fee, :total_amount,
              :discription, :duration, :created_at, :updated_at
 end
